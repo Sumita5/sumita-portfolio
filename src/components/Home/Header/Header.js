bae37c61f85f2a36c2fc14a83327af1b7de01ac5
Sumita5 <satyakiroyhreeg@gmail.com>
2020-11-01 18:15:43 +0600

    project done

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
-        <div style={{height:"700px"}} className="header container-fluid">
+        <div style={{height:"700px"}} className="headerBackground container-fluid">
             <Navbar/>
             <Headermain/>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About/About';
+import Blog from './Blog/Blog';
 import Header from './Header/Header';
 import './Home.css';
 import Projects from './Projects/Projects';
@@ -9,6 +10,7 @@ const Home = () => {
         <div className="home">
           <Header/>
           <Projects/>
+          <Blog/>
           <About/>
         </div>
     );
